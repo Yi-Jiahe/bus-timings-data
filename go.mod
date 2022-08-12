77fb3d4288a10518aa9ed309e8d59dd5428f3aab
Yi Jiahe <yi.jiahe00@gmail.com>
2022-08-12 16:56:33 +0800

    query datamall for bustimings w/ datamall wrapper dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Yi-Jiahe/bus-timings-data
 
 go 1.19
+
+require github.com/yi-jiayu/datamall/v3 v3.1.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+
+	"github.com/yi-jiayu/datamall/v3"
 )
 
 func main() {
 	API_ACCOUNT_KEY := os.Getenv("API_ACCOUNT_KEY")
-	print(API_ACCOUNT_KEY)
+
+	busStopCode := flag.String("stop", "", "5 Digit Bus Stop Code")
+	service := flag.String("service", "", "Service")
+	flag.Parse()
+
+	fmt.Printf("Querying arrivals for %s at %s\n", *busStopCode, *service)
+
+	c := datamall.NewDefaultClient(API_ACCOUNT_KEY)
+
+	arrival, err := c.GetBusArrival(*busStopCode, *service)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v+\n", arrival)
 }
